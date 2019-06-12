96db3469343761e2758b3317cd9141004178d57b
LeeYounJi <48303096+YOUNJILEE@users.noreply.github.com>
2019-06-12 22:29:52 +0900

    21861095이윤지

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
-console.log("aaaa");
+
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.hideme').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'margin-left':'0px'},1000);
+            }
+            
+        }); 
+    });
+});
